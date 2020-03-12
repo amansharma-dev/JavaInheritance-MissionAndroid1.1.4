bf97d35f385cbde0a361f893758d3ecf8b5e7e36
amansharma-dev <amansharma.11@hotmail.com>
2020-03-12 13:32:31 +0530

    new commit javainheritance

--- JavaInheritance/src/main/java/com/example/javainheritance/Manager.java
+++ JavaInheritance/src/main/java/com/example/javainheritance/Manager.java
@@ -4,6 +4,12 @@ public class Manager extends Employee {
 
     private String teamAllocated;
 
+    //override super class
+    @Override
+    public double getSalary() {
+        return super.getSalary() + 10000;
+    }
+
     public Manager(){}
     public Manager(String teamAllocated){
         this.teamAllocated = teamAllocated;
@@ -15,5 +21,14 @@ public class Manager extends Employee {
 
     public void setTeamAllocated(String teamAllocated) {
         this.teamAllocated = teamAllocated;
+    }
+
+    // to string override
+
+    @Override
+    public String toString() {
+        return "First Name: "+this.getFirstName()+", Last Name: "+this.getLastName()+", Type: "+this.getType()
+                +",Working Period: "+this.getWorkingPeriod()+", Age: "+this.getAge()+", Team Allocated: "+this.getTeamAllocated()
+                +", Salary: "+this.getSalary();
     }
 }
--- JavaInheritance/src/main/java/com/example/javainheritance/MyClass.java
+++ JavaInheritance/src/main/java/com/example/javainheritance/MyClass.java
@@ -4,6 +4,25 @@ public class MyClass {
 
     public static void main(String[] args) {
 
+        //using String as a Object
+        String message = "hello";
+        boolean isEmpty = message.isEmpty();
+        boolean contains = message.contains("h");
+
+        if(!contains){
+            System.out.println("no h found");
+        }
+        else{
+            System.out.println("h found in message, yahoo....");
+        }
+
+        if(!isEmpty){
+            System.out.println(message);
+        }
+        else{
+            System.out.println("Empty");
+        }
+
         Employee employee = new Employee();
         employee.setFirstName("James");
         employee.setLastName("Mathews");
@@ -20,8 +39,13 @@ public class MyClass {
         manager.setFirstName("Will");
         manager.setLastName("Smith");
         manager.setType("Manager");
+        manager.setAge(29);
+        manager.setWorkingPeriod(2.5f);
         manager.setTeamAllocated("Sales");
+        manager.setSalary(970000);
 
-        System.out.println("First Name: "+manager.getFirstName()+" & Last Name: "+manager.getLastName()+", Employee Type: "+manager.getType()+", Team Allocated: "+manager.getTeamAllocated());
+       // System.out.println("First Name: "+manager.getFirstName()+" & Last Name: "+manager.getLastName()+", Employee Type: "+manager.getType()+", Team Allocated: "+manager.getTeamAllocated()+",  Salary is: "+manager.getSalary());
+
+        System.out.println(manager);
     }
 }
